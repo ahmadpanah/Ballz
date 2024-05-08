540bddfe6fd7ee61cec3ba994a0fb50ee2f611e0
Hossein <djalicrt@gmail.com>
2024-05-08 11:12:19 +0330

    Add Bounce and Pysics Material to Ball

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Paddle : MonoBehaviour
 
   public float speed = 30f;
   
+  public float maxBounceAngle = 75f;
     private void Awake() {
         this.rigidbody = GetComponent<Rigidbody2D>();
     }
@@ -26,5 +28,24 @@ public class Paddle : MonoBehaviour
      if (this.direction != Vector2.zero) {
       this.rigidbody.AddForce(this.direction * this.speed);
      } 
+     
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+      Ball ball = collision.gameObject.GetComponent<Ball>();
+      if (ball != null) {
+        Vector3 paddlePosition = this.transform.position;
+        Vector2 contactPoint = collision.GetContact(0).point;
+
+        float offset = paddlePosition.x - contactPoint.x;
+        float width = collision.otherCollider.bounds.size.x / 2;
+
+        float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+        float bounceAngle = (offset / width) * this.maxBounceAngle;
+        float newAngle = Mathf.Clamp(currentAngle + bounceAngle,-this.maxBounceAngle,maxBounceAngle);
+
+       Quaternion rotation = Quaternion.AngleAxis(newAngle,Vector3.forward);
+       ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
+      }
     }
 }
